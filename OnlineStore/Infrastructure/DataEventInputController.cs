1eb554e5592e035d471c50afc3499233a3458fd3
Alex Sudakov <alexdsa@yandex.ru>
2020-04-08 11:17:52 +0300

    1117

---
+++ OnlineStore/Infrastructure/DataEventInputController.cs
@@ -0,0 +1,14 @@
+﻿using OnlineStore.Infrastructure.EventBus;
+
+namespace OnlineStore.Infrastructure
+{
+    public abstract class DataEventInputController
+    {
+        private IEventBus _eventBus;
+
+        protected DataEventInputController (IEventBus eventBus)
+        {
+            _eventBus = eventBus;
+        }
+    }
+}
